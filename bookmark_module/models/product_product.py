36242d23fefaeb7aff84120eeb22f2b5f3be2faf
samiha996 <144477577+samiha996@users.noreply.github.com>
2025-03-28 12:22:22 +0200

    Update product_product.py

--- bookmark_module/models/product_product.py
+++ bookmark_module/models/product_product.py
@@ -5,21 +5,21 @@ class ProductProduct(models.Model):
 
     department_id = fields.Many2one('department', string='Department')
     serie_id = fields.Many2one('serie', string='Serie')
-    # x_publisher = fields.Char(
-    #     related='product_tmpl_id.x_Publisher',
-    #     store=True,
-    #     readonly=False
-    # )
-    # x_main_supplier = fields.Char(
-    #     related='product_tmpl_id.x_Main_Supplier',
-    #     store=True,
-    #     readonly=False
-    # )
-    # x_authors = fields.Char(
-    #     related='product_tmpl_id.x_Authors',
-    #     store=True,
-    #     readonly=False
-    # )
+    x_publisher = fields.Char(
+        related='product_tmpl_id.x_Publisher',
+        store=True,
+        readonly=False
+    )
+    x_main_supplier = fields.Char(
+        related='product_tmpl_id.x_Main_Supplier',
+        store=True,
+        readonly=False
+    )
+    x_authors = fields.Char(
+        related='product_tmpl_id.x_Authors',
+        store=True,
+        readonly=False
+    )
     school_id = fields.Many2one(
         'product.school',
         related='product_tmpl_id.school_id',
