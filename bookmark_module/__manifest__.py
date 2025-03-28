8dc6c8f4a73a0ff704a3ecdbdf8061806670990b
samiha996 <144477577+samiha996@users.noreply.github.com>
2025-03-28 12:57:36 +0200

    Update __manifest__.py

--- bookmark_module/__manifest__.py
+++ bookmark_module/__manifest__.py
@@ -7,7 +7,7 @@
         Bookmark Module
     """,
     'author': 'Samiha',
-    'depends': ['base','product','purchase','sale','product_school'],   
+    'depends': ['base','product','purchase','sale'],   
     'data': [
         'security/ir.model.access.csv',
         'views/product_product_form_view.xml',
