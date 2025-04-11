1a4989feb9d946c6a4424c7f906fbdd57664657a
samiha996 <144477577+samiha996@users.noreply.github.com>
2025-04-11 16:31:00 +0300

    Update __init__.py

--- bookmark_module/models/__init__.py
+++ bookmark_module/models/__init__.py
@@ -1,5 +1,7 @@
 from . import department
 from . import book_author
+from . import publisher
+from . import main_distributor
 from . import product_template
 from . import product_product
 from . import serie
