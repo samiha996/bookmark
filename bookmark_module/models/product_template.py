880492c064fd8240e1a1c59e7882c1a90ed50811
samiha996 <144477577+samiha996@users.noreply.github.com>
2025-04-11 16:36:26 +0300

    Update product_template.py

--- bookmark_module/models/product_template.py
+++ bookmark_module/models/product_template.py
@@ -12,4 +12,6 @@ class ProductTemplate(models.Model):
     subtitle = fields.Char(string="Subtitle")
     subtitle2 = fields.Char(string="Subtitle 2")
     date_of_publication = fields.Date(string="Date of Publication")
+    publisher = fields.Many2one('publisher', string="Publisher")
+    main_distributor = fields.Many2one('main.distributor', string="Main Distributor")
     
