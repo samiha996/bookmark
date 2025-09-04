b9533c7e85eaa3225d7df470cdc8a6b6021ae8cf
samiha996 <144477577+samiha996@users.noreply.github.com>
2025-09-04 16:31:46 +0300

    Update website_pre_booking.py

--- website_pre_booking/controllers/website_pre_booking.py
+++ website_pre_booking/controllers/website_pre_booking.py
@@ -1,24 +1,4 @@
 # -*- coding: utf-8 -*-
-#############################################################################
-#
-#    Cybrosys Technologies Pvt. Ltd.
-#
-#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
-#    Author: Vishnu KP(<https://www.cybrosys.com>)
-#
-#    You can modify it under the terms of the GNU AFFERO
-#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
-#
-#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
-#    (AGPL v3) along with this program.
-#    If not, see <http://www.gnu.org/licenses/>.
-#
-#############################################################################
 from odoo import fields, http, _
 from odoo.http import request
 
@@ -30,56 +10,87 @@ class CustomerPortal(http.Controller):
                 type='http', auth="public", website=True)
     def portal_my_employee_request(self, product, **kwargs):
         """Pre-book button to pre-book the product"""
-        vals = {
-            'product': product.id,
-        }
-        current_user = request.env['res.users'].sudo().browse(
-            request.session.uid)
-        partner = current_user.partner_id
-        product_qty = int(kwargs.get('prod_qty'))
+        # --- Defensive parse for quantity ---
+        raw_qty = kwargs.get('prod_qty')
+        try:
+            # Accept floats but store as int; default to 1 if missing/invalid
+            product_qty = int(float(raw_qty)) if raw_qty not in (None, "",) else 1
+        except Exception:
+            product_qty = 1
+        if product_qty < 1:
+            product_qty = 1
+
+        # Validate available pre-book quantity if configured
+        max_quantity = int(product.pre_max_quantity or 0)
+        if max_quantity and product_qty > max_quantity:
+            # Exceeds allowed quantity: show fail page
+            return request.render("website_pre_booking.pre_booking_failed")
+
+        vals = {'product': product.id, 'prod_qty': product_qty}
+
+        # Logged-in user flow
         if request.session.uid:
+            current_user = request.env['res.users'].sudo().browse(request.session.uid)
+            partner = current_user.partner_id
 
             pre_booking = request.env['website.prebook'].sudo().create({
                 'partner_id': partner.id,
-                'booking_date': fields.datetime.today(),
+                'booking_date': fields.Date.today(),
                 'product_id': product.id,
                 'quantity': product_qty,
                 'website_id': request.website.id,
             })
-            if pre_booking:
-                max_quantity = product.pre_max_quantity
-                product.sudo().write({'pre_max_quantity': int(max_quantity - product_qty)})
+            if pre_booking and max_quantity:
+                product.sudo().write({'pre_max_quantity': max_quantity - product_qty})
 
             return request.render("website_pre_booking.pre_booking_done",
                                   {'ref': pre_booking.reference})
+        # Public (not logged in): send to address form and keep qty
         else:
             return request.render("website_pre_booking.prebook_address", vals)
 
     @http.route(['/prebook/address'], type='http', methods=['GET', 'POST'],
                 auth="public", website=True, sitemap=False)
     def pre_address(self, **kw):
-        print('If not login create a new user', **kw)
-        """If not login, create a new user"""
-        product = request.env['product.template'].sudo().browse(
-            int(kw.get('product')))
+        """If not login, create a new partner and pre-book with provided qty"""
+        # Defensive read for product and quantity
+        product_id = int(kw.get('product') or 0)
+        product = request.env['product.template'].sudo().browse(product_id)
+        raw_qty = kw.get('prod_qty')
+        try:
+            qty = int(float(raw_qty)) if raw_qty not in (None, "",) else 1
+        except Exception:
+            qty = 1
+        if qty < 1:
+            qty = 1
+
+        # Validate against max quantity
+        max_quantity = int(product.pre_max_quantity or 0)
+        if max_quantity and qty > max_quantity:
+            return request.render("website_pre_booking.pre_booking_failed")
+
         partner = request.env['res.partner'].sudo().create({
             'name': kw.get('name'),
             'email': kw.get('email'),
             'phone': kw.get('phone'),
         })
+
         pre_booking = request.env['website.prebook'].sudo().create({
             'partner_id': partner.id,
-            'booking_date': fields.datetime.today(),
-            'product_id': product.id})
-        if pre_booking:
-            max_quantity = product.pre_max_quantity
-            product.pre_max_quantity = max_quantity - 1
+            'booking_date': fields.Date.today(),
+            'product_id': product.id,
+            'quantity': qty,
+            'website_id': request.website.id,
+        })
+        if pre_booking and max_quantity:
+            product.sudo().write({'pre_max_quantity': max_quantity - qty})
+
         return request.render("website_pre_booking.pre_booking_done",
                               {'ref': pre_booking.reference})
 
     @http.route('/track/prebooking', website=True, auth='user', csrf=False)
     def submit_booking(self, **kwargs):
-        """For tracking the specific pre-orders using refernce code"""
+        """For tracking the specific pre-orders using reference code"""
         bookings = request.env['website.prebook'].sudo().search(
             [('reference', '=', kwargs.get('reference'))])
         if bookings and bookings.sale_id:
@@ -99,11 +110,9 @@ class CustomerPortal(http.Controller):
                 'status': state if bookings.sale_id else bookings.state,
                 'date': bookings.booking_date,
             }
-            return request.render("website_pre_booking.my_booking_template",
-                                  vals)
+            return request.render("website_pre_booking.my_booking_template", vals)
         else:
-            return request.render("website_pre_booking.my_booking_template",
-                                  {'vals': True})
+            return request.render("website_pre_booking.my_booking_template", {'vals': True})
 
     @http.route(['/my/prebookings', '/my/prebookings/page/<int:page>'],
                 type='http', auth="user", website=True)
@@ -115,5 +124,5 @@ class CustomerPortal(http.Controller):
 
     @http.route(['/sale/fail'], type='http', auth="user", website=True)
     def my_prebookings_fail(self):
-        """Can track the pre bookings from the website"""
+        """Failure page"""
         return request.render("website_pre_booking.pre_booking_failed")
